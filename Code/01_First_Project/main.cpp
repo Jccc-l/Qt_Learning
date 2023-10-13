9358b5f591a1f543afd748df93256cc4cf85030d
Jccc-l <1216550215Jc@gmail.com>
2023-10-13 10:46:54 +0800

    Add comments for "01_First_Project"

--- Code/01_First_Project/main.cpp
+++ Code/01_First_Project/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+    QApplication a(argc, argv);		// 应用程序对象，在Qt中只有一个
+    Widget w;   			// 窗口对象
+    w.show();   			// 调用show方法显示窗口
+    return a.exec();			// 让应用程序进入消息循环
 }
